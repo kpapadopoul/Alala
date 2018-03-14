9cf771dec8a1df127c37c3a88921ff604a6cd511
Konstantinos Papadopoulos <k.papadopoulos@torawallet.gr>
2018-03-14 16:53:34 +0200

    Add business partner-related project.

---
+++ AlalaBusinessPartners/AlalaBusinessPartners/Controllers/BusinessPartnerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using SAPbobsCOM;
+
+using AlalaBusinessPartners.Models;
+
+namespace AlalaBusinessPartners.Controllers
+{
+    class BusinessPartnerController
+    {
+        private readonly Company _company;
+
+        public BusinessPartnerController(Company company)
+        {
+            _company = company;
+        }
+
+        public void Create(BusinessPartnerModel businessPartner)
+        {
+            // Prepare the object
+            var bp = (BusinessPartners)_company.GetBusinessObject(BoObjectTypes.oBusinessPartners);
+
+            // Set values
+            // The built-in auto-complete procedure completes the default values of the other properties.
+            bp.CardCode = businessPartner.Code; //Mandatory
+            bp.CardName = businessPartner.Name;
+            bp.CardType = BoCardTypes.cCustomer; //Mandatory
+
+            // Add it to the database
+            var success = bp.Add().Equals(0);
+            if (!success)
+            {
+                // Error handling
+                int code;
+                string msg;
+                _company.GetLastError(out code, out msg);
+                throw new Exception($"Something went wrong\n{code} {msg}");
+            }
+        }
+    }
+}
---
+++ AlalaBusinessPartners/AlalaBusinessPartners/Models/BusinessPartnerModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AlalaBusinessPartners.Models
+{
+    public class BusinessPartnerModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
