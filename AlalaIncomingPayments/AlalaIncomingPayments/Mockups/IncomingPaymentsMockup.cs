ed982732afb692884363d182af33c08b7afb7146
Konstantinos Papadopoulos <k.papadopoulos@torawallet.gr>
2018-06-08 13:55:03 +0300

    ALALA-18: Update AlalaIncomingPayments project to add mockup where needed.

---
+++ AlalaIncomingPayments/AlalaIncomingPayments/Mockups/IncomingPaymentsMockup.cs
@@ -0,0 +1,11 @@
+﻿using AlalaIncomingPayments.Models;
+
+namespace AlalaIncomingPayments.Mockups
+{
+    public class IncomingPaymentsMockup : Interfaces.IIncomingPayments
+    {
+        public IncomingPaymentModel GetById(int incomingPaymentEntry) { return new IncomingPaymentModel(); }
+        public void Create(IncomingPaymentModel incomingPayment) { }
+        public bool Delete(int incomingPaymentEntry) { return false; }
+    }
+}
