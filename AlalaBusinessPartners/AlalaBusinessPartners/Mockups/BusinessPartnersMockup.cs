21621e29b5c20c3f3aa7c061752c66b51f61d8f5
Konstantinos Papadopoulos <k.papadopoulos@torawallet.gr>
2018-06-08 13:31:09 +0300

    ALALA-15: Update AlalaBusinessPartners project to add mockup where needed.

---
+++ AlalaBusinessPartners/AlalaBusinessPartners/Mockups/BusinessPartnersMockup.cs
@@ -0,0 +1,12 @@
+﻿using AlalaBusinessPartners.Models;
+
+namespace AlalaBusinessPartners.Mockups
+{
+    public class BusinessPartnersMockup : Interfaces.IBusinessPartners
+    {
+        public BusinessPartnerModel GetById(string businessPartnerCode) { return new BusinessPartnerModel(); }
+        public void Create(BusinessPartnerModel businessPartner) { }
+        public bool UpdateContactEmployees(string businessPartnerCode, BusinessPartnerModel businessPartner) { return false; }
+        public bool Delete(string businessPartnerCode) { return false; }
+    }
+}
