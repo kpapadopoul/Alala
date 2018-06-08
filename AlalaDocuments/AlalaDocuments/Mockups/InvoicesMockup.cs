c325495f5163c08792af004cee15a4ee2b239a7f
Konstantinos Papadopoulos <k.papadopoulos@torawallet.gr>
2018-06-08 13:49:10 +0300

    ALALA-17: Update AlalaDocuments project to add mockup where needed.

---
+++ AlalaDocuments/AlalaDocuments/Mockups/InvoicesMockup.cs
@@ -0,0 +1,13 @@
+﻿using AlalaDocuments.Models;
+
+namespace AlalaDocuments.Mockups
+{
+    public class InvoicesMockup : Interfaces.IInvoices
+    {
+        public InvoiceModel GetById(int docEntry) { return new InvoiceModel(); }
+        public void Create(InvoiceModel invoice) { }
+        public void CreateBasedOnOrder(int orderId, InvoiceModel invoice) { }
+        public bool UpdateItems(int docEntry, InvoiceModel invoice) { return false; }
+        public bool Delete(int docEntry) { return false; }
+    }
+}
---
+++ AlalaDocuments/AlalaDocuments/Mockups/OrdersMockup.cs
@@ -0,0 +1,12 @@
+﻿using AlalaDocuments.Models;
+
+namespace AlalaDocuments.Mockups
+{
+    public class OrdersMockup : Interfaces.IOrders
+    {
+        public OrderModel GetById(int docEntry) { return new OrderModel(); }
+        public void Create(OrderModel order) { }
+        public bool UpdateItems(int docEntry, OrderModel order) { return false; }
+        public bool Delete(int docEntry) { return false; }
+    }
+}
