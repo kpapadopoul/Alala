172065229139968357a5f5e4b88c882c45a986a3
Konstantinos Papadopoulos <k.papadopoulos@torawallet.gr>
2018-06-08 13:38:48 +0300

    ALALA-16: Update AlalaJournalEntries project to add mockup where needed.

---
+++ AlalaJournalEntries/AlalaJournalEntries/Mockups/JournalEntriesMockup.cs
@@ -0,0 +1,11 @@
+﻿using AlalaJournalEntries.Models;
+
+namespace AlalaJournalEntries.Mockups
+{
+    public class JournalEntriesMockup : Interfaces.IJournalEntries
+    {
+        public JournalEntryModel GetById(int jdtNum) { return new JournalEntryModel(); }
+        public void Create(JournalEntryModel journalEntry) { }
+        public bool Delete(int jdtNum) { return false; }
+    }
+}
